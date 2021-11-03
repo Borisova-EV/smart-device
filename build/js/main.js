6bed05bb5caccec8e06174e83acce987ecb0901a
Евгения Борисова <borisova.ev.2019@yandex.ru>
2021-11-03 12:14:11 +0300

    Добавляет блок о компании

--- build/js/main.js
+++ build/js/main.js
@@ -1,15 +1,22 @@
 const accordionButtons = document.querySelectorAll('.page-footer__accordion-button');
 const accordionContents = document.querySelectorAll('.page-footer__accordion-content');
+const sliceText = document.querySelector('.about__slice-text');
+const text = sliceText.textContent;
 
-function closeAccordionContent () {
+let smallDevice = window.matchMedia("(max-width: 1023px)");
+
+const START_INDEX_SLICE_TEXT = 0;
+const END_INDEX_SLICE_TEXT = 208;
+
+function closeAccordionContent() {
   accordionContents.forEach(function (elem) {
     elem.classList.add('page-footer__accordion-content--hidden');
   })
 
 }
 
-function openAccordionMenu () {
-  closeAccordionContent ();
+function openAccordionMenu() {
+  closeAccordionContent();
   accordionButtons.forEach(function (button) {
     button.addEventListener('click', function (evt) {
       const buttonCurrent = evt.target;
@@ -32,4 +39,25 @@ function openAccordionMenu () {
   })
 }
 
-openAccordionMenu ();
+openAccordionMenu();
+
+function truncateString(str, num) {
+  if (str.length <= num) {
+    return str
+  }
+  return str.slice(START_INDEX_SLICE_TEXT, num) + '..'
+}
+
+function cutParagraph() {
+  let newText;
+  if (smallDevice.matches) {
+   newText = truncateString(text, END_INDEX_SLICE_TEXT);
+   } else {
+     newText = text;
+   }
+    sliceText.textContent = newText;
+}
+
+cutParagraph();
+window.addEventListener('resize', () => cutParagraph());
+
