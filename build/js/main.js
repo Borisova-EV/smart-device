8b0601a871a9c67c4d21a9af392892c0da205cae
Евгения Борисова <borisova.ev.2019@yandex.ru>
2021-11-05 20:57:37 +0300

    Добавляет маску для набора телефона

--- build/js/main.js
+++ build/js/main.js
@@ -2,20 +2,30 @@ const accordionButtons = document.querySelectorAll('.page-footer__accordion-butt
 const accordionContents = document.querySelectorAll('.page-footer__accordion-content');
 const sliceText = document.querySelector('.about__slice-text');
 const text = sliceText.textContent;
+const form = document.querySelector('.form');
+const inputRequired = document.querySelectorAll('input[required]');
+const inputName = document.querySelector('input#name');
+const inputTelephone = document.querySelector('input#telephone');
+const inputMessage = document.querySelector('textarea');
 
 let smallDevice = window.matchMedia("(max-width: 1023px)");
+let isStorageSupport = true;
+let storageName = '';
+let storageTelephone = '';
+let storageMessage = '';
 
 const START_INDEX_SLICE_TEXT = 0;
 const END_INDEX_SLICE_TEXT = 208;
+const FIRST_SYMBOL = '+7(';
+const POSITION_PARENTHESIS_MASK = 7;
 
 function closeAccordionContent() {
   accordionContents.forEach(function (elem) {
     elem.classList.add('page-footer__accordion-content--hidden');
   })
-
 }
 
-function changeButtonAccordion (currentButton) {
+function changeButtonAccordion(currentButton) {
   accordionButtons.forEach(function (button) {
     if (button !== currentButton) {
       button.classList.remove('page-footer__accordion-button--current');
@@ -28,12 +38,10 @@ function openAccordionMenu() {
   accordionButtons.forEach(function (button) {
     button.addEventListener('click', function (evt) {
       const buttonCurrent = evt.target;
-      changeButtonAccordion (buttonCurrent);
-      console.log(evt.target);
+      changeButtonAccordion(buttonCurrent);
       buttonCurrent.classList.toggle('page-footer__accordion-button--current');
       const content = buttonCurrent.nextElementSibling;
       accordionContents.forEach(function (elem) {
-        console.log(4);
         if (elem === content) {
           elem.classList.toggle('page-footer__accordion-content--hidden');
         } else {
@@ -56,13 +64,73 @@ function truncateString(str, num) {
 function cutParagraph() {
   let newText;
   if (smallDevice.matches) {
-   newText = truncateString(text, END_INDEX_SLICE_TEXT);
-   } else {
-     newText = text;
-   }
-    sliceText.textContent = newText;
+    newText = truncateString(text, END_INDEX_SLICE_TEXT);
+  } else {
+    newText = text;
+  }
+  sliceText.textContent = newText;
 }
 
 cutParagraph();
 window.addEventListener('resize', () => cutParagraph());
 
+
+try {
+  storageName = localStorage.getItem('name');
+} catch (err) {
+  isStorageSupport = false;
+}
+try {
+  storageTelephone = localStorage.getItem('telephone');
+} catch (err) {
+  isStorageSupport = false;
+}
+try {
+  storageMessage = localStorage.getItem('message');
+} catch (err) {
+  isStorageSupport = false;
+}
+
+
+function isRequiredInputEmpty() {
+  inputRequired.forEach(function (input) {
+    input.value === 0
+  })
+}
+
+function submitForm() {
+  form.addEventListener('submit', function (evt) {
+    if (isRequiredInputEmpty) {
+      evt.preventDefault();
+    } else if (isStorageSupport) {
+      localStorage.setItem('name', inputName.value);
+      localStorage.setItem('telephone', inputTelephone.value);
+      localStorage.setItem('message', inputMessage.value);
+    }
+  })
+}
+
+submitForm();
+
+inputTelephone.addEventListener('focus', function () {
+  inputTelephone.value = FIRST_SYMBOL;
+})
+
+function createTelephoneMask() {
+  inputTelephone.addEventListener('input', function (evt) {
+    if (inputTelephone.value == 0) {
+      inputTelephone.value = FIRST_SYMBOL + inputTelephone.value;
+    } else {
+      if (/[0-9]/.test(evt.data)) {
+        if (inputTelephone.value.length >= (POSITION_PARENTHESIS_MASK - 1) && inputTelephone.value.length < POSITION_PARENTHESIS_MASK) {
+          inputTelephone.value += ')';
+        }
+      } else {
+        const position = evt.target.selectionStart;
+        inputTelephone.value = inputTelephone.value.substring(0, position - 1);
+      }
+    }
+  })
+}
+
+createTelephoneMask();
